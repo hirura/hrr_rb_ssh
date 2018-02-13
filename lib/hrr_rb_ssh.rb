b6a3ce5e0cfe4b1668c4ae63f16fa85e7cab75f7
hirura <hirura@gmail.com>
2018-02-13 14:19:18 +0900

    update existing files to specify encoding and to add vim modeline

--- lib/hrr_rb_ssh.rb
+++ lib/hrr_rb_ssh.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
 require "hrr_rb_ssh/version"
 
 module HrrRbSsh
--- lib/hrr_rb_ssh/version.rb
+++ lib/hrr_rb_ssh/version.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
 module HrrRbSsh
   VERSION = "0.1.0"
 end
