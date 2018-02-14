0d94f4d7cb872efb248213e4d4099af9ec47d2d5
hirura <hirura@gmail.com>
2018-02-14 13:03:10 +0900

    add logger

--- lib/hrr_rb_ssh.rb
+++ lib/hrr_rb_ssh.rb
@@ -2,6 +2,7 @@
 # vim: et ts=2 sw=2
 
 require "hrr_rb_ssh/version"
+require "hrr_rb_ssh/logger"
 
 module HrrRbSsh
   # Your code goes here...
---
+++ lib/hrr_rb_ssh/logger.rb
@@ -0,0 +1,48 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+module HrrRbSsh
+  class Logger
+    def self.initialize logger
+      @@logger = logger
+    end
+
+    def self.initialized?
+      @@logger != nil
+    end
+
+    def initialize name
+      @name = name
+    end
+
+    def fatal message
+      if self.class.initialized?
+        @@logger.fatal "#{@name}: #{message}"
+      end
+    end
+
+    def error message
+      if self.class.initialized?
+        @@logger.error "#{@name}: #{message}"
+      end
+    end
+
+    def warn message
+      if self.class.initialized?
+        @@logger.warn "#{@name}: #{message}"
+      end
+    end
+
+    def info message
+      if self.class.initialized?
+        @@logger.info "#{@name}: #{message}"
+      end
+    end
+
+    def debug message
+      if self.class.initialized?
+        @@logger.debug "#{@name}: #{message}"
+      end
+    end
+  end
+end
