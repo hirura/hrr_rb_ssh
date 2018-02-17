68c5328e81adc9b09703b350d53c1c145d217cb6
hirura <hirura@gmail.com>
2018-02-17 20:04:41 +0900

    update spec for transport/sequence_number

--- spec/hrr_rb_ssh-transport-sequence_number_spec.rb
+++ spec/hrr_rb_ssh-transport-sequence_number_spec.rb
@@ -24,42 +24,45 @@ RSpec.describe HrrRbSsh::Transport::SequenceNumber do
     end
 
     context 'Until 2^32' do
-      it "has sequence_number 1 when called once" do
-        sequence_number.increment
-        expect( sequence_number.sequence_number ).to eq 1
+      it "is changed from 0 to 1 when called once" do
+        expect { sequence_number.increment }.to change { sequence_number.sequence_number }.from( 0 ).to( 1 )
       end
 
-      it "has sequence_number 100000 when called 100000 times" do
-        100000.times do
-          sequence_number.increment
-        end
-        expect( sequence_number.sequence_number ).to eq 100000
+      it "is changed from 0 to 100000 when called 100000 times" do
+        expect {
+          100000.times do
+            sequence_number.increment
+          end
+        }.to change { sequence_number.sequence_number }.from( 0 ).to( 100000 )
       end
 
-      it "has sequence_number 2^32 - 1 when called 2 times when sequence_number is 2^32 - 3" do
+      it "is changed from 2^32 - 3 to 2^32 - 1 when called 2 times" do
         sequence_number.sequence_number = 2 ** 32 - 3
-        2.times do
-          sequence_number.increment
-        end
-        expect( sequence_number.sequence_number ).to eq 2 ** 32 - 1
+        expect {
+          2.times do
+            sequence_number.increment
+          end
+        }.to change { sequence_number.sequence_number }.from( 2 ** 32 - 3 ).to( 2 ** 32 - 1 )
       end
     end
 
     context 'Over 2^32' do
-      it "has sequence_number 0 when called once when sequence_number is 2^32 - 1" do
+      it "is changed from 2^32 - 1 to 0 when called once" do
         sequence_number.sequence_number = 2 ** 32 - 1
-        1.times do
-          sequence_number.increment
-        end
-        expect( sequence_number.sequence_number ).to eq 0
+        expect {
+          1.times do
+            sequence_number.increment
+          end
+        }.to change { sequence_number.sequence_number }.from( 2 ** 32 - 1 ).to( 0 )
       end
 
-      it "has sequence_number 1 when called 2 times when sequence_number is 2^32 - 1" do
+      it "is changed from 2^32 - 1 to 1 when called 2 times" do
         sequence_number.sequence_number = 2 ** 32 - 1
-        2.times do
-          sequence_number.increment
-        end
-        expect( sequence_number.sequence_number ).to eq 1
+        expect {
+          2.times do
+            sequence_number.increment
+          end
+        }.to change { sequence_number.sequence_number }.from( 2 ** 32 - 1 ).to( 1 )
       end
     end
   end
