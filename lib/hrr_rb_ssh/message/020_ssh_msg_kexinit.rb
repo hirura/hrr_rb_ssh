af9b2fbbc0c619f6a6659b5894142a4512f6d4d7
hirura <hirura@gmail.com>
2018-02-20 09:37:37 +0900

    update message/020_ssh_msg_kexinit module and class structure

--- lib/hrr_rb_ssh/message/020_ssh_msg_kexinit.rb
+++ lib/hrr_rb_ssh/message/020_ssh_msg_kexinit.rb
@@ -6,46 +6,50 @@ require 'hrr_rb_ssh/message/codable'
 
 module HrrRbSsh
   module Message
-    class SSH_MSG_KEXINIT
-      extend Codable
+    module SSH_MSG_KEXINIT
+      class << self
+        include Codable
 
-      ID    ||= self.name.split('::').last
-      VALUE ||= 20
-
-      def self.definition
-        [
-          # [Data Type, Field Name]
-          ['byte',      'SSH_MSG_KEXINIT'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['byte',      'cookie (random byte)'],
-          ['name-list', 'kex_algorithms'],
-          ['name-list', 'server_host_key_algorithms'],
-          ['name-list', 'encryption_algorithms_client_to_server'],
-          ['name-list', 'encryption_algorithms_server_to_client'],
-          ['name-list', 'mac_algorithms_client_to_server'],
-          ['name-list', 'mac_algorithms_server_to_client'],
-          ['name-list', 'compression_algorithms_client_to_server'],
-          ['name-list', 'compression_algorithms_server_to_client'],
-          ['name-list', 'languages_client_to_server'],
-          ['name-list', 'languages_server_to_client'],
-          ['boolean',   'first_kex_packet_follows'],
-          ['uint32',    '0 (reserved for future extension)'],
-        ]
+        def definition
+          DEFINITION
+        end
       end
+
+      ID    = self.name.split('::').last
+      VALUE = 20
+
+      DEFINITION = [
+        # [Data Type, Field Name]
+        ['byte',      'SSH_MSG_KEXINIT'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['byte',      'cookie (random byte)'],
+        ['name-list', 'kex_algorithms'],
+        ['name-list', 'server_host_key_algorithms'],
+        ['name-list', 'encryption_algorithms_client_to_server'],
+        ['name-list', 'encryption_algorithms_server_to_client'],
+        ['name-list', 'mac_algorithms_client_to_server'],
+        ['name-list', 'mac_algorithms_server_to_client'],
+        ['name-list', 'compression_algorithms_client_to_server'],
+        ['name-list', 'compression_algorithms_server_to_client'],
+        ['name-list', 'languages_client_to_server'],
+        ['name-list', 'languages_server_to_client'],
+        ['boolean',   'first_kex_packet_follows'],
+        ['uint32',    '0 (reserved for future extension)'],
+      ]
     end
   end
 end
