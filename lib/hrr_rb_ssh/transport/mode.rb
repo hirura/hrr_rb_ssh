3d45bf0c7f13c452786b382e0624e6d94dba88b3
hirura <hirura@gmail.com>
2018-02-21 21:38:45 +0900

    add transport/mode and its spec

---
+++ lib/hrr_rb_ssh/transport/mode.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+module HrrRbSsh
+  class Transport
+    module Mode
+      SERVER = :server
+      CLIENT = :client
+    end
+  end
+end
---
+++ spec/hrr_rb_ssh/transport/mode_spec.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+RSpec.describe HrrRbSsh::Transport::Mode do
+  let(:server){ :server }
+  let(:client){ :client }
+
+  it "has SERVER" do
+    expect(described_class::SERVER).to eq :server
+  end
+
+  it "has CLIENT" do
+    expect(described_class::CLIENT).to eq :client
+  end
+end
