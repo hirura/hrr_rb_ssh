c69ff8fee98e31db78149a0f01e4d3187f937116
hirura <hirura@gmail.com>
2018-02-21 22:09:28 +0900

    add transport/server_host_key_algorithm and its spec

---
+++ lib/hrr_rb_ssh/transport/server_host_key_algorithm.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+require 'hrr_rb_ssh/logger'
+
+module HrrRbSsh
+  class Transport
+    class ServerHostKeyAlgorithm
+      @@list ||= Hash.new
+
+      def self.[] key
+        @@list[key]
+      end
+
+      def self.name_list
+        @@list.keys
+      end
+    end
+  end
+end
