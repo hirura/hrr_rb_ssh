953b112d7966d881d0db56419613cd79b24f9704
hirura <hirura@gmail.com>
2018-02-24 15:20:01 +0900

    update transport spec to check initialized variables

--- spec/hrr_rb_ssh/transport_spec.rb
+++ spec/hrr_rb_ssh/transport_spec.rb
@@ -6,8 +6,44 @@ RSpec.describe HrrRbSsh::Transport do
   let(:mode){ 'server' }
 
   describe '#initialize' do
+    let(:transport){ described_class.new io, mode }
+
     it "takes two arguments: io and mode" do
-      expect { HrrRbSsh::Transport.new io, mode }.not_to raise_error
+      expect { transport }.not_to raise_error
+    end
+
+    it "initializes incoming_sequence_number readable" do
+      expect(transport.incoming_sequence_number).to be_an_instance_of HrrRbSsh::Transport::SequenceNumber
+      expect(transport.incoming_sequence_number.sequence_number).to eq 0
+    end
+
+    it "initializes outgoing_sequence_number readable" do
+      expect(transport.outgoing_sequence_number).to be_an_instance_of HrrRbSsh::Transport::SequenceNumber
+      expect(transport.outgoing_sequence_number.sequence_number).to eq 0
+    end
+
+    it "initializes incoming_encryption_algorithm readable" do
+      expect(transport.incoming_encryption_algorithm).to be_an_instance_of HrrRbSsh::Transport::EncryptionAlgorithm::None
+    end
+
+    it "initializes incoming_mac_algorithm readable" do
+      expect(transport.incoming_mac_algorithm).to be_an_instance_of HrrRbSsh::Transport::MacAlgorithm::None
+    end
+
+    it "initializes incoming_compression_algorithm readable" do
+      expect(transport.incoming_compression_algorithm).to be_an_instance_of HrrRbSsh::Transport::CompressionAlgorithm::None
+    end
+
+    it "initializes outgoing_encryption_algorithm readable" do
+      expect(transport.outgoing_encryption_algorithm).to be_an_instance_of HrrRbSsh::Transport::EncryptionAlgorithm::None
+    end
+
+    it "initializes outgoing_mac_algorithm readable" do
+      expect(transport.outgoing_mac_algorithm).to be_an_instance_of HrrRbSsh::Transport::MacAlgorithm::None
+    end
+
+    it "initializes outgoing_compression_algorithm readable" do
+      expect(transport.outgoing_compression_algorithm).to be_an_instance_of HrrRbSsh::Transport::CompressionAlgorithm::None
     end
   end
 end
