c4d27ba72776514a1d210e458a809d9a4dc90c01
hirura <hirura@gmail.com>
2018-03-03 23:34:27 +0900

    fix transport/kex_algorithm to re-register diffie_hellman_group1_sha1

--- lib/hrr_rb_ssh/transport/kex_algorithm.rb
+++ lib/hrr_rb_ssh/transport/kex_algorithm.rb
@@ -2,6 +2,7 @@
 # vim: et ts=2 sw=2
 
 require 'hrr_rb_ssh/logger'
+require 'hrr_rb_ssh/transport/kex_algorithm/diffie_hellman_group1_sha1'
 require 'hrr_rb_ssh/transport/kex_algorithm/diffie_hellman_group14_sha1'
 
 module HrrRbSsh
