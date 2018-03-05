44be75fabfdc42d42d4cd69cddd772b7ec5e30a5
hirura <hirura@gmail.com>
2018-03-05 23:23:52 +0900

    add message/005_ssh_msg_service_request and 006_ssh_msg_service_accept and their specs

--- lib/hrr_rb_ssh/message.rb
+++ lib/hrr_rb_ssh/message.rb
@@ -1,6 +1,8 @@
 # coding: utf-8
 # vim: et ts=2 sw=2
 
+require 'hrr_rb_ssh/message/005_ssh_msg_service_request'
+require 'hrr_rb_ssh/message/006_ssh_msg_service_accept'
 require 'hrr_rb_ssh/message/020_ssh_msg_kexinit'
 require 'hrr_rb_ssh/message/021_ssh_msg_newkeys'
 require 'hrr_rb_ssh/message/030_ssh_msg_kexdh_init'
---
+++ spec/hrr_rb_ssh/message/005_ssh_msg_service_request_spec.rb
@@ -0,0 +1,44 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+RSpec.describe HrrRbSsh::Message::SSH_MSG_SERVICE_REQUEST do
+  let(:id){ 'SSH_MSG_SERVICE_REQUEST' }
+  let(:value){ 5 }
+
+  describe "::ID" do
+    it "is defined" do
+      expect(described_class::ID).to eq id
+    end
+  end
+
+  describe "::VALUE" do
+    it "is defined" do
+      expect(described_class::VALUE).to eq value
+    end
+  end
+
+  let(:message){
+    {
+      id             => value,
+      'service name' => 'ssh-userauth',
+    }
+  }
+  let(:payload){
+    [
+      HrrRbSsh::Transport::DataType::Byte.encode(message[id]),
+      HrrRbSsh::Transport::DataType::String.encode(message['service name']),
+    ].join
+  }
+
+  describe ".encode" do
+    it "returns payload encoded" do
+      expect(described_class.encode(message)).to eq payload
+    end
+  end
+
+  describe ".decode" do
+    it "returns message decoded" do
+      expect(described_class.decode(payload)).to eq message
+    end
+  end
+end
