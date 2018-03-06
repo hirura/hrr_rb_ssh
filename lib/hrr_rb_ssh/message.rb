bb2654d1df47ff6060f5fe0e9f3bc3e897bd57f7
hirura <hirura@gmail.com>
2018-03-06 19:17:34 +0900

    add message/060_ssh_msg_userauth_pk_ok and its spec

--- lib/hrr_rb_ssh/message.rb
+++ lib/hrr_rb_ssh/message.rb
@@ -10,6 +10,7 @@ require 'hrr_rb_ssh/message/031_ssh_msg_kexdh_reply'
 require 'hrr_rb_ssh/message/050_ssh_msg_userauth_request'
 require 'hrr_rb_ssh/message/051_ssh_msg_userauth_failure'
 require 'hrr_rb_ssh/message/052_ssh_msg_userauth_success'
+require 'hrr_rb_ssh/message/060_ssh_msg_userauth_pk_ok'
 
 module HrrRbSsh
   module Message
---
+++ spec/hrr_rb_ssh/message/060_ssh_msg_userauth_pk_ok_spec.rb
@@ -0,0 +1,46 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+RSpec.describe HrrRbSsh::Message::SSH_MSG_USERAUTH_PK_OK do
+  let(:id){ 'SSH_MSG_USERAUTH_PK_OK' }
+  let(:value){ 60 }
+
+  describe "::ID" do
+    it "is defined" do
+      expect(described_class::ID).to eq id
+    end
+  end
+
+  describe "::VALUE" do
+    it "is defined" do
+      expect(described_class::VALUE).to eq value
+    end
+  end
+
+  let(:message){
+    {
+      id                                           => value,
+      'public key algorithm name from the request' => 'dummy',
+      'public key blob from the request'           => 'dummy',
+    }
+  }
+  let(:payload){
+    [
+      HrrRbSsh::Transport::DataType::Byte.encode(message[id]),
+      HrrRbSsh::Transport::DataType::String.encode(message['public key algorithm name from the request']),
+      HrrRbSsh::Transport::DataType::String.encode(message['public key blob from the request']),
+    ].join
+  }
+
+  describe ".encode" do
+    it "returns payload encoded" do
+      expect(described_class.encode(message)).to eq payload
+    end
+  end
+
+  describe ".decode" do
+    it "returns message decoded" do
+      expect(described_class.decode(payload)).to eq message
+    end
+  end
+end
