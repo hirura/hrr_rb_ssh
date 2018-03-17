cb3781aaffcc9d125c3ae04c24ed92110e3cd855
hirura <hirura@gmail.com>
2018-03-17 15:09:55 +0900

    update authentication to implement authenticate loop

--- lib/hrr_rb_ssh/authentication.rb
+++ lib/hrr_rb_ssh/authentication.rb
@@ -21,6 +21,47 @@ module HrrRbSsh
 
     def start
       @transport.start
+
+      authenticate
+    end
+
+    def authenticate
+      loop do
+        payload = @transport.receive
+        case payload[0,1].unpack("C")[0]
+        when HrrRbSsh::Message::SSH_MSG_USERAUTH_REQUEST::VALUE
+          userauth_request_message = HrrRbSsh::Message::SSH_MSG_USERAUTH_REQUEST.decode payload
+          method_name = userauth_request_message['method name']
+          method = Method[method_name].new(@options)
+          if method.authenticate(userauth_request_message)
+            send_userauth_success
+            break
+          else
+            send_userauth_failure
+          end
+
+        else
+          raise
+        end
+      end
+    end
+
+    def send_userauth_failure
+      message = {
+        'SSH_MSG_USERAUTH_FAILURE'          => HrrRbSsh::Message::SSH_MSG_USERAUTH_FAILURE::VALUE,
+        'authentications that can continue' => Method.name_list,
+        'partial success'                   => false,
+      }
+      payload = HrrRbSsh::Message::SSH_MSG_USERAUTH_FAILURE.encode message
+      @transport.send payload
+    end
+
+    def send_userauth_success
+      message = {
+        'SSH_MSG_USERAUTH_SUCCESS' => HrrRbSsh::Message::SSH_MSG_USERAUTH_SUCCESS::VALUE,
+      }
+      payload = HrrRbSsh::Message::SSH_MSG_USERAUTH_SUCCESS.encode message
+      @transport.send payload
     end
   end
 end
--- spec/hrr_rb_ssh/authentication_spec.rb
+++ spec/hrr_rb_ssh/authentication_spec.rb
@@ -39,10 +39,129 @@ RSpec.describe HrrRbSsh::Authentication do
     let(:transport){ HrrRbSsh::Transport.new io, mode }
     let(:authentication){ described_class.new(transport) }
 
-    it "calls transport.start" do
+    it "calls transport.start and authenticate methods" do
       expect( transport ).to receive(:start).with(no_args).once
+      expect( authentication ).to receive(:authenticate).with(no_args).once
 
       authentication.start
+    end
+  end
+
+  describe '#authenticate' do
+    let(:io){ 'dummy' }
+    let(:mode){ 'dummy' }
+    let(:transport){ HrrRbSsh::Transport.new io, mode }
+    let(:authentication){ described_class.new(transport, options) }
+    let(:userauth_failure_message){
+      {
+        "SSH_MSG_USERAUTH_FAILURE"          => 51,
+        'authentications that can continue' => HrrRbSsh::Authentication::Method.name_list,
+        'partial success'                   => false,
+      }
+    }
+    let(:userauth_failure_payload){
+      HrrRbSsh::Message::SSH_MSG_USERAUTH_FAILURE.encode userauth_failure_message
+    }
+    let(:userauth_success_message){
+      {
+        "SSH_MSG_USERAUTH_SUCCESS" => 52,
+      }
+    }
+    let(:userauth_success_payload){
+      HrrRbSsh::Message::SSH_MSG_USERAUTH_SUCCESS.encode userauth_success_message
+    }
+
+    context "when accept none method" do
+      let(:options){
+        {
+          'authentication_none_authenticator' => HrrRbSsh::Authentication::Authenticator.new { true },
+        }
+      }
+      let(:userauth_request_with_none_method_message){
+        {
+          "SSH_MSG_USERAUTH_REQUEST" => 50,
+          "user name"                => "username",
+          "service name"             => "ssh-connection",
+          "method name"              => "none",
+        }
+      }
+      let(:userauth_request_with_none_method_payload){
+        HrrRbSsh::Message::SSH_MSG_USERAUTH_REQUEST.encode userauth_request_with_none_method_message
+      }
+
+      it "receives" do
+        expect( transport ).to receive(:receive).with(no_args).and_return(userauth_request_with_none_method_payload).once
+        expect( transport ).to receive(:send).with(userauth_success_payload).once
+
+        authentication.authenticate
+      end
+    end
+
+    context "when accept password method after none method" do
+      let(:options){
+        {
+          'authentication_none_authenticator' => HrrRbSsh::Authentication::Authenticator.new { false },
+          'authentication_password_authenticator' => HrrRbSsh::Authentication::Authenticator.new { |context| context.verify 'username', 'password' },
+        }
+      }
+      let(:userauth_request_with_none_method_message){
+        {
+          "SSH_MSG_USERAUTH_REQUEST" => 50,
+          "user name"                => "username",
+          "service name"             => "ssh-connection",
+          "method name"              => "none",
+        }
+      }
+      let(:userauth_request_with_none_method_payload){
+        HrrRbSsh::Message::SSH_MSG_USERAUTH_REQUEST.encode userauth_request_with_none_method_message
+      }
+      let(:userauth_request_with_password_method_message){
+        {
+          "SSH_MSG_USERAUTH_REQUEST" => 50,
+          "user name"                => "username",
+          "service name"             => "ssh-connection",
+          "method name"              => "password",
+          "FALSE"                    => false, 
+          "plaintext password"       => "password"
+        }
+      }
+      let(:userauth_request_with_password_method_payload){
+        HrrRbSsh::Message::SSH_MSG_USERAUTH_REQUEST.encode userauth_request_with_password_method_message
+      }
+      let(:userauth_requests){
+        [
+          userauth_request_with_none_method_payload,
+          userauth_request_with_password_method_payload,
+        ]
+      }
+
+      it "receives" do
+        expect( transport ).to receive(:receive).with(no_args).and_return(*userauth_requests).twice
+        expect( transport ).to receive(:send).with(userauth_failure_payload).once
+        expect( transport ).to receive(:send).with(userauth_success_payload).once
+
+        authentication.authenticate
+      end
+    end
+
+    context "when receives not userauth request" do
+      let(:options){ {} }
+      let(:not_userauth_request_message){
+        {
+          "NOT_USERAUTH_REQUEST" => 123,
+        }
+      }
+      let(:not_userauth_request_payload){
+        [
+          HrrRbSsh::Transport::DataType::Byte.encode(not_userauth_request_message.values[0]),
+        ].join
+      }
+
+      it "receives" do
+        expect( transport ).to receive(:receive).with(no_args).and_return(not_userauth_request_payload).once
+
+        expect { authentication.authenticate }.to raise_error RuntimeError
+      end
     end
   end
 end
