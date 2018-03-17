d74776f96d8e12c6ef95519f0f5c21b03981e551
hirura <hirura@gmail.com>
2018-03-17 19:51:51 +0900

    update message/codable and its spec to support complementary message to encode and decode

--- lib/hrr_rb_ssh/message/codable.rb
+++ lib/hrr_rb_ssh/message/codable.rb
@@ -23,16 +23,16 @@ module HrrRbSsh
         }
       end
 
-      def encode message
+      def encode message, complementary_message={}
         logger.debug('encoding message: ' + message.inspect)
-        definition = common_definition + conditional_definition(message)
+        definition = common_definition + conditional_definition(message.merge complementary_message)
         definition.map{ |data_type, field_name|
           field_value = if message[field_name].instance_of? ::Proc then message[field_name].call else message[field_name] end
           HrrRbSsh::Transport::DataType[data_type].encode( field_value )
         }.join
       end
 
-      def decode payload
+      def decode payload, complementary_message={}
         def decode_recursively payload_io, message=nil
           if message.class == Array and message.size == 0
             []
@@ -43,20 +43,24 @@ module HrrRbSsh
                          when Array
                            conditional_definition(message)
                          end
-            additional_message = definition.map{ |data_type, field_name|
+            decoded_message = definition.map{ |data_type, field_name|
               [
                 field_name,
                 HrrRbSsh::Transport::DataType[data_type].decode( payload_io )
               ]
             }
 
-            additional_message + decode_recursively(payload_io, additional_message)
+            decoded_message + decode_recursively(payload_io, decoded_message)
           end
         end
 
-        message = decode_recursively(StringIO.new payload).to_h
-        logger.debug('decoded message: ' + message.inspect)
-        message
+        payload_io = StringIO.new payload
+        decoded_message = decode_recursively(payload_io).to_h
+        if complementary_message.any?
+          decoded_message.merge! decode_recursively(payload_io, complementary_message.to_a).to_h
+        end
+        logger.debug('decoded message: ' + decoded_message.inspect)
+        decoded_message
       end
     end
   end
--- spec/hrr_rb_ssh/message/codable_spec.rb
+++ spec/hrr_rb_ssh/message/codable_spec.rb
@@ -145,4 +145,53 @@ RSpec.describe HrrRbSsh::Message::Codable do
       end
     end
   end
+
+  context "when module has CONDITIONAL_DEFINITION that requires complementary message" do
+    before :context do
+      module SSH_MSG_MOCK_WITH_HIDDEN_CONDITIONAL_DEFINITION
+        class << self
+          include HrrRbSsh::Message::Codable
+        end
+
+        DEFINITION = [
+          ['byte',   'SSH_MSG_MOCK'],
+          ['string', 'data'        ],
+        ]
+
+        HIDDEN_DEFINITION = [
+          ['string', 'hidden data'],
+        ]
+
+        CONDITIONAL_DEFINITION = {
+          'require hidden' => {
+            true => HIDDEN_DEFINITION,
+          },
+        }
+      end
+    end
+
+    let(:mixed_in){
+      SSH_MSG_MOCK_WITH_HIDDEN_CONDITIONAL_DEFINITION
+    }
+
+    describe ".encode" do
+      context "when arg does not contain an instance of Proc" do
+        it "encodes #{{'SSH_MSG_MOCK' => 168, 'data' => 'testing', 'hidden data' => 'conditional'}.inspect} with complementary message #{{'require hidden' => true}.inspect} to \"A8 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\"" do
+          expect( mixed_in.encode( {'SSH_MSG_MOCK' => 168, 'data' => 'testing', 'hidden data' => 'conditional'}, {'require hidden' => true} ) ).to eq( ["A8", "00000007", "testing", "0000000B", "conditional"].pack("H*H*a*H*a*") )
+        end
+      end
+
+      context "when arg contains an instance of Proc" do
+        it "encodes #{{'SSH_MSG_MOCK' => 168, 'data' => lambda { 'testing' }, 'hidden data' => lambda { 'conditional' }}.inspect} with complementary message #{{'require hidden' => lambda { true }}.inspect} to \"A8 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\"" do
+          expect( mixed_in.encode( {'SSH_MSG_MOCK' => 168, 'data' => lambda { 'testing' }, 'hidden data' => lambda { 'conditional' }}, {'require hidden' => lambda { true }} ) ).to eq( ["A8", "00000007", "testing", "0000000B", "conditional"].pack("H*H*a*H*a*") )
+        end
+      end
+    end
+
+    describe ".decode" do
+      it "decodes \"A8 00 00 00 07 t e s t i n g 00 00 00 0B c o n d i t i o n a l\" with complementary message #{{'require hidden' => true}.inspect} to #{{'SSH_MSG_MOCK' => 168, 'data' => 'testing', 'hidden data' => 'conditional'}.inspect}" do
+        expect( mixed_in.decode( ["A8", "00000007", "testing", "0000000B", "conditional"].pack("H*H*a*H*a*"), {'require hidden' => true} ) ).to eq( {'SSH_MSG_MOCK' => 168, 'data' => 'testing', 'hidden data' => 'conditional'} )
+      end
+    end
+  end
 end
