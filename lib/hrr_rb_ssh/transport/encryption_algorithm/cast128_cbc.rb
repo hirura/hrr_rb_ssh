68bc9b8e5a665c7f80771bffbff3e4be9bcfacad
hirura <hirura@gmail.com>
2018-04-06 22:49:27 +0900

    add cast128-cbc encryption algorithms

---
+++ lib/hrr_rb_ssh/transport/encryption_algorithm/cast128_cbc.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+require 'hrr_rb_ssh/transport/encryption_algorithm/encryption_algorithm'
+require 'hrr_rb_ssh/transport/encryption_algorithm/functionable'
+
+module HrrRbSsh
+  class Transport
+    class EncryptionAlgorithm
+      class Cast128Cbc < EncryptionAlgorithm
+        NAME        = 'cast128-cbc'
+        CIPHER_NAME = "CAST5-CBC"
+        BLOCK_SIZE  = 8
+
+        include Functionable
+      end
+    end
+  end
+end
