285a66a70bcc2b3ee25a343a0302f88f2de1725f
hirura <hirura@gmail.com>
2018-04-06 23:31:35 +0900

    update transport/mac_algorithm/hmac_sha1 spec for future various algorithm support

--- spec/hrr_rb_ssh/transport/mac_algorithm/hmac_sha1_spec.rb
+++ spec/hrr_rb_ssh/transport/mac_algorithm/hmac_sha1_spec.rb
@@ -2,34 +2,42 @@
 # vim: et ts=2 sw=2
 
 RSpec.describe HrrRbSsh::Transport::MacAlgorithm::HmacSha1 do
-  let(:key){ [Array.new(20){ |i| "%02x" % i }.join].pack("H*") }
+  let(:name){ 'hmac-sha1' }
+  let(:digest){ 'sha1' }
+  let(:digest_length){ 20 }
+  let(:key_length){ 20 }
+  let(:key){ [Array.new(key_length){ |i| "%02x" % i }.join].pack("H*") }
   let(:mac_algorithm){ described_class.new key }
   let(:sequence_number){ 0 }
   let(:unencrypted_packet){ "testing" }
 
-  it "is registered as hmac-sha1 in list of HrrRbSsh::Transport::MacAlgorithm" do
-    expect( HrrRbSsh::Transport::MacAlgorithm['hmac-sha1'] ).to eq described_class
+  it "is registered in HrrRbSsh::Transport::MacAlgorithm.list" do
+    expect( HrrRbSsh::Transport::MacAlgorithm.list ).to include described_class
   end
 
-  it "appears as hmac-sha1 in HrrRbSsh::Transport::MacAlgorithm.name_list" do
-    expect( HrrRbSsh::Transport::MacAlgorithm.name_list ).to include 'hmac-sha1'
+  it "can be looked up in HrrRbSsh::Transport::MacAlgorithm dictionary" do
+    expect( HrrRbSsh::Transport::MacAlgorithm[name] ).to eq described_class
   end
 
-  describe '#compute' do
-    it "returns 94 c9 a1 af 46 02 92 a7 06 8d 10 9e 63 f8 e0 8b 86 18 96 f8" do
-      expect( mac_algorithm.compute sequence_number, unencrypted_packet ).to eq ["94c9a1af460292a7068d109e63f8e08b861896f8"].pack("H*")
-    end
+  it "appears in HrrRbSsh::Transport::MacAlgorithm.name_list" do
+    expect( HrrRbSsh::Transport::MacAlgorithm.name_list ).to include name
   end
 
   describe '#digest_length' do
-    it "returns 20" do
-      expect( mac_algorithm.digest_length ).to eq 20
+    it "returns expected digest length" do
+      expect( mac_algorithm.digest_length ).to eq digest_length
     end
   end
 
   describe '#key_length' do
-    it "returns 20" do
-      expect( mac_algorithm.key_length ).to eq 20
+    it "returns expected key length" do
+      expect( mac_algorithm.key_length ).to eq key_length
+    end
+  end
+
+  describe '#compute' do
+    it "returns expected digest" do
+      expect( mac_algorithm.compute sequence_number, unencrypted_packet ).to eq OpenSSL::HMAC.digest(digest, key, ([sequence_number].pack("N") + unencrypted_packet))
     end
   end
 end
