f22878e04430b54cd49c35b59dd1656947f9a675
hirura <hirura@gmail.com>
2018-04-06 22:24:14 +0900

    add aesXXX-ctr encryption algorithms

---
+++ lib/hrr_rb_ssh/transport/encryption_algorithm/aes128_ctr.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+require 'hrr_rb_ssh/transport/encryption_algorithm/encryption_algorithm'
+require 'hrr_rb_ssh/transport/encryption_algorithm/functionable'
+
+module HrrRbSsh
+  class Transport
+    class EncryptionAlgorithm
+      class Aes128Ctr < EncryptionAlgorithm
+        NAME        = 'aes128-ctr'
+        CIPHER_NAME = "AES-128-CTR"
+        BLOCK_SIZE  = 16
+
+        include Functionable
+      end
+    end
+  end
+end
---
+++ lib/hrr_rb_ssh/transport/encryption_algorithm/aes256_ctr.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+require 'hrr_rb_ssh/transport/encryption_algorithm/encryption_algorithm'
+require 'hrr_rb_ssh/transport/encryption_algorithm/functionable'
+
+module HrrRbSsh
+  class Transport
+    class EncryptionAlgorithm
+      class Aes256Ctr < EncryptionAlgorithm
+        NAME        = 'aes256-ctr'
+        CIPHER_NAME = "AES-256-CTR"
+        BLOCK_SIZE  = 16
+
+        include Functionable
+      end
+    end
+  end
+end
