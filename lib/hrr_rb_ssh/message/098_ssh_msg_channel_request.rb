79e22aede603e9e679a0c8cc595da0426fd7c1ac
hirura <hirura@gmail.com>
2018-04-29 13:21:09 +0900

    add terminal mode definitions in channel request message

--- lib/hrr_rb_ssh/message/098_ssh_msg_channel_request.rb
+++ lib/hrr_rb_ssh/message/098_ssh_msg_channel_request.rb
@@ -23,12 +23,73 @@ module HrrRbSsh
         USR2 = 'USR2'
       end
 
+      module TerminalMode
+        TTY_OP_END    =   0
+        VINTR         =   1
+        VQUIT         =   2
+        VERASE        =   3
+        VKILL         =   4
+        VEOF          =   5
+        VEOL          =   6
+        VEOL2         =   7
+        VSTART        =   8
+        VSTOP         =   9
+        VSUSP         =  10
+        VDSUSP        =  11
+        VREPRINT      =  12
+        VWERASE       =  13
+        VLNEXT        =  14
+        VFLUSH        =  15
+        VSWTCH        =  16
+        VSTATUS       =  17
+        VDISCARD      =  18
+        IGNPAR        =  30
+        PARMRK        =  31
+        INPCK         =  32
+        ISTRIP        =  33
+        INLCR         =  34
+        IGNCR         =  35
+        ICRNL         =  36
+        IUCLC         =  37
+        IXON          =  38
+        IXANY         =  39
+        IXOFF         =  40
+        IMAXBEL       =  41
+        ISIG          =  50
+        ICANON        =  51
+        XCASE         =  52
+        ECHO          =  53
+        ECHOE         =  54
+        ECHOK         =  55
+        ECHONL        =  56
+        NOFLSH        =  57
+        TOSTOP        =  58
+        IEXTEN        =  59
+        ECHOCTL       =  60
+        ECHOKE        =  61
+        PENDIN        =  62
+        OPOST         =  70
+        OLCUC         =  71
+        ONLCR         =  72
+        OCRNL         =  73
+        ONOCR         =  74
+        ONLRET        =  75
+        CS7           =  90
+        CS8           =  91
+        PARENB        =  92
+        PARODD        =  93
+        TTY_OP_ISPEED = 128
+        TTY_OP_OSPEED = 129
+      end
+
       class << self
         include Codable
       end
 
       ID    = self.name.split('::').last
       VALUE = 98
+
+      TERMINAL_MODE_INV = TerminalMode.constants.map{|c| [TerminalMode.const_get(c), c.to_s]}.to_h
 
       DEFINITION = [
         #[DataType, Field Name]
