9a93f7e64ad3d2147b63db64ed8a1d7d69afa1f4
hirura <hirura@gmail.com>
2018-05-29 20:19:46 +0900

    update top-level module to require sub-modules/classes after declaring

--- lib/hrr_rb_ssh.rb
+++ lib/hrr_rb_ssh.rb
@@ -1,6 +1,10 @@
 # coding: utf-8
 # vim: et ts=2 sw=2
 
+module HrrRbSsh
+  # Your code goes here...
+end
+
 require "hrr_rb_ssh/version"
 require "hrr_rb_ssh/compat"
 require "hrr_rb_ssh/logger"
@@ -11,7 +15,3 @@ require "hrr_rb_ssh/transport"
 require "hrr_rb_ssh/authentication"
 require "hrr_rb_ssh/connection"
 require "hrr_rb_ssh/server"
-
-module HrrRbSsh
-  # Your code goes here...
-end
