b8485f799453c4fdcc2690d1299fee380065cddf
hirura <hirura@gmail.com>
2018-05-29 20:56:53 +0900

    add comments in hrr_rb_ssh/data_type/byte

--- lib/hrr_rb_ssh/data_type/byte.rb
+++ lib/hrr_rb_ssh/data_type/byte.rb
@@ -3,7 +3,13 @@
 
 module HrrRbSsh
   class DataType
+    # Byte provides methods to convert integer value and 8-bit unsigned binary string each other.
     class Byte < DataType
+      # Convert integer value info 8-bit unsigned binary string.
+      #
+      # @param [::Integer] arg integer value to be converted
+      # @raise [::ArgumentError] when arg is not between 0x00 and 0xff
+      # @return [::String] converted 8-bit unsigned binary string
       def self.encode arg
         case arg
         when 0x00..0xff
@@ -13,6 +19,10 @@ module HrrRbSsh
         end
       end
 
+      # Convert 8-bit unsigned binary into Integer value.
+      #
+      # @param [::IO] io IO instance that has buffer to be read
+      # @return [::Integer] converted integer value
       def self.decode io
         io.read(1).unpack("C")[0]
       end
