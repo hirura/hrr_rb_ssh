d31f7a602e80af4010430946b4a4bbf847271c9e
hirura <hirura@gmail.com>
2018-05-30 22:10:05 +0900

    add comments in hrr_rb_ssh/data_type/string

--- lib/hrr_rb_ssh/data_type/string.rb
+++ lib/hrr_rb_ssh/data_type/string.rb
@@ -3,7 +3,14 @@
 
 module HrrRbSsh
   class DataType
+    # String provides methods to convert Ruby's string value and binary string each other.
     class String < DataType
+      # Convert Ruby's string value into binary string.
+      #
+      # @param [::String] arg Ruby's string value to be converted
+      # @raise [::ArgumentError] when arg is not string
+      # @raise [::ArgumentError] when length of arg is longer than 0xffff_ffff
+      # @return [::String] converted binary string
       def self.encode arg
         unless arg.kind_of? ::String
           raise ArgumentError, "must be a kind of String, but got #{arg.inspect}"
@@ -14,6 +21,10 @@ module HrrRbSsh
         [arg.length, arg].pack("Na*")
       end
 
+      # Convert binary string into Ruby's string value.
+      #
+      # @param [::IO] io IO instance that has buffer to be read
+      # @return [::String] converted Ruby's string value
       def self.decode io
         length = io.read(4).unpack("N")[0]
         io.read(length).unpack("a*")[0]
