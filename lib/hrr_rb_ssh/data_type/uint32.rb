5722f6fbd19413d23372435661c8d96fc8fe4cc0
hirura <hirura@gmail.com>
2018-05-30 22:10:20 +0900

    add comments in hrr_rb_ssh/data_type/uint32

--- lib/hrr_rb_ssh/data_type/uint32.rb
+++ lib/hrr_rb_ssh/data_type/uint32.rb
@@ -3,7 +3,13 @@
 
 module HrrRbSsh
   class DataType
+    # Uint32 provides methods to convert integer value and 32-bit unsigned binary string each other.
     class Uint32 < DataType
+      # Convert integer value into 32-bit unsigned binary string.
+      #
+      # @param [::Integer] arg integer value to be converted
+      # @raise [::ArgumentError] when arg is not between 0x0000_0000 and 0xffff_ffff
+      # @return [::String] converted 32-bit unsigned binary string
       def self.encode arg
         case arg
         when 0x0000_0000..0xffff_ffff
@@ -13,6 +19,10 @@ module HrrRbSsh
         end
       end
 
+      # Convert 32-bit unsigned binary into Integer value.
+      #
+      # @param [::IO] io IO instance that has buffer to be read
+      # @return [::Integer] converted integer value
       def self.decode io
         io.read(4).unpack("N")[0]
       end
