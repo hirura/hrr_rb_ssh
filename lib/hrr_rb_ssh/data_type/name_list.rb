8a4b15fd51ee8792c3e20b3d88df169aefde5997
hirura <hirura@gmail.com>
2018-05-30 22:10:12 +0900

    add comments in hrr_rb_ssh/data_type/name_list

--- lib/hrr_rb_ssh/data_type/name_list.rb
+++ lib/hrr_rb_ssh/data_type/name_list.rb
@@ -3,7 +3,14 @@
 
 module HrrRbSsh
   class DataType
+    # NameList provides methods to convert a comma-separated list of names and binary string each other.
     class NameList < DataType
+      # Convert a comma-separated list of names into binary string.
+      #
+      # @param [::Array] arg an array that containes names to be converted
+      # @raise [::ArgumentError] when arg is not an array
+      # @raise [::ArgumentError] when arg array containes an instance of not string
+      # @return [::String] converted binary string
       def self.encode arg
         unless arg.kind_of? Array
           raise ArgumentError, "must be a kind of Array, but got #{arg.inspect}"
@@ -18,6 +25,10 @@ module HrrRbSsh
         [joined_arg.length, joined_arg].pack("Na*")
       end
 
+      # Convert binary string into a comma-separated list of names.
+      #
+      # @param [::IO] io IO instance that has buffer to be read
+      # @return [::Array] converted a comma-separated list of names
       def self.decode io
         length = io.read(4).unpack("N")[0]
         io.read(length).unpack("a*")[0].split(',')
