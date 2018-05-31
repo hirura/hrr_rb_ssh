6ab6ad552b581ede39e2b990b3c0e2ec6b01aadb
hirura <hirura@gmail.com>
2018-05-31 20:59:56 +0900

    add connection/request_handler/reference_env_request_handler spec

---
+++ spec/hrr_rb_ssh/connection/request_handler/reference_env_request_handler_spec.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+RSpec.describe HrrRbSsh::Connection::RequestHandler::ReferenceEnvRequestHandler do
+  describe ".new" do
+    it "does not take any arguments" do
+      expect { described_class.new("arg") }.to raise_error ArgumentError
+    end
+  end
+
+  describe "#run" do
+    let(:request_handler){ described_class.new }
+
+    let(:context){
+      HrrRbSsh::Connection::Channel::ChannelType::Session::RequestType::Env::Context.new proc_chain, username, io, variables, message
+    }
+
+    let(:proc_chain){
+      double('proc_chain')
+    }
+    let(:username){
+      double('username')
+    }
+    let(:io){
+      double('io')
+    }
+    let(:variables){
+      {}
+    }
+    let(:message){
+      {
+        :'variable name'  => variable_name,
+        :'variable value' => variable_value,
+      }
+    }
+
+    let(:variable_name ){ 'variable name'  }
+    let(:variable_value){ 'variable value' }
+
+    it "calls proc with context argument" do
+      request_handler.run context
+      expect(variables[:env]).to eq ({variable_name => variable_value})
+    end
+  end
+end
