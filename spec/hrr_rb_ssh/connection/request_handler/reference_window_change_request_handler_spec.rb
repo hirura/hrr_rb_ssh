6d90be62571f4294ec58f27b10af4ce95f1ffe7c
hirura <hirura@gmail.com>
2018-05-31 21:13:50 +0900

    add connection/request_handler/reference_window_change_request_handler spec

---
+++ spec/hrr_rb_ssh/connection/request_handler/reference_window_change_request_handler_spec.rb
@@ -0,0 +1,55 @@
+# coding: utf-8
+# vim: et ts=2 sw=2
+
+RSpec.describe HrrRbSsh::Connection::RequestHandler::ReferenceWindowChangeRequestHandler do
+  describe ".new" do
+    it "does not take any arguments" do
+      expect { described_class.new("arg") }.to raise_error ArgumentError
+    end
+  end
+
+  describe "#run" do
+    let(:request_handler){ described_class.new }
+
+    let(:context){
+      HrrRbSsh::Connection::Channel::ChannelType::Session::RequestType::WindowChange::Context.new proc_chain, username, io, variables, message
+    }
+
+    let(:proc_chain){
+      double('proc_chain')
+    }
+    let(:username){
+      double('username')
+    }
+    let(:io){
+      double('io')
+    }
+    let(:variables){
+      {
+        :ptm => ptm,
+      }
+    }
+    let(:message){
+      {
+        :'terminal width, columns' => 200,
+        :'terminal height, rows'   => 100,
+        :'terminal width, pixels'  => 2000,
+        :'terminal height, pixels' => 1000,
+      }
+    }
+
+    let(:ptm_pts){ PTY.open }
+    let(:ptm){ ptm_pts[0] }
+    let(:pts){ ptm_pts[1] }
+
+    after :example do
+      ptm.close rescue nil
+      pts.close rescue nil
+    end
+
+    it "calls proc with context argument" do
+      request_handler.run context
+      expect( ptm.winsize ).to eq [100, 200]
+    end
+  end
+end
