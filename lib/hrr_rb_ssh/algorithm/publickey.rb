0a2d9cdc0587c175726f976e3432efc86485d321
hirura <hirura@gmail.com>
2018-06-03 16:06:01 +0900

    update algorithm/publickey.rb connection/channel/channel_type.rb connection/channel/channel_type/session/request_type.rb to use subclass_without_preference_listable

--- lib/hrr_rb_ssh/algorithm/publickey.rb
+++ lib/hrr_rb_ssh/algorithm/publickey.rb
@@ -1,25 +1,14 @@
 # coding: utf-8
 # vim: et ts=2 sw=2
 
+require 'hrr_rb_ssh/subclass_without_preference_listable'
+
 module HrrRbSsh
   module Algorithm
     class Publickey
       @subclass_list = Array.new
       class << self
-        def inherited klass
-          @subclass_list.push klass if @subclass_list
-        end
-
-        def [] key
-          __subclass_list__(__method__).find{ |klass| klass::NAME == key }
-        end
-
-        def __subclass_list__ method_name
-          send(:method_missing, method_name) unless @subclass_list
-          @subclass_list
-        end
-
-        private :__subclass_list__
+        include SubclassWithoutPreferenceListable
       end
     end
   end
--- lib/hrr_rb_ssh/connection/channel/channel_type/session/request_type.rb
+++ lib/hrr_rb_ssh/connection/channel/channel_type/session/request_type.rb
@@ -1,6 +1,8 @@
 # coding: utf-8
 # vim: et ts=2 sw=2
 
+require 'hrr_rb_ssh/subclass_without_preference_listable'
+
 module HrrRbSsh
   class Connection
     class Channel
@@ -9,20 +11,7 @@ module HrrRbSsh
           class RequestType
             @subclass_list = Array.new
             class << self
-              def inherited klass
-                @subclass_list.push klass if @subclass_list
-              end
-
-              def [] key
-                __subclass_list__(__method__).find{ |klass| klass::NAME == key }
-              end
-
-              def __subclass_list__ method_name
-                send(:method_missing, method_name) unless @subclass_list
-                @subclass_list
-              end
-
-              private :__subclass_list__
+              include SubclassWithoutPreferenceListable
             end
           end
         end
